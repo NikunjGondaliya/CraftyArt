5b199a1a62b9efd79fcfff5c32d7a9c32d272979
Deep Rathod <deeprathod74@gmail.com>
2023-03-16 01:27:51 +0530

    Done Routing in remaining pages

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,11 @@ import ContactUs from "./pages/contactus/contactus";
 import CreateBlog from "./pages/createblog/createblog";
 import Invoice from "./pages/invoice/invoice";
 import PricePlan from "./pages/pricePlan/pricePlan";
+import AboutUs from "./pages/aboutus/aboutus";
+import Blog from "./pages/blog/blog";
+import Career from "./pages/career/carrer";
+import Subscriptions from "./pages/subscriptions/subscriptions";
+import Successful from "./pages/successfull/successfull";
 
 function App() {
   const location = useLocation();
@@ -32,6 +37,11 @@ function App() {
         <Route path="/invoice" element={<Invoice />} />
         <Route path="/pricePlans" element={<PricePlan />} />
         <Route path="/term-condition" element={<TermCondition />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/career" element={<Career />} />
+        <Route path="/subscriptions" element={<Subscriptions />} />
+        <Route path="/successfull" element={<Successful />} />
       </Routes>
       <Footer />
     </>
