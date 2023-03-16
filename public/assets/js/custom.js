d2fc52a887309847c2f749ece0ad45503a7d8388
nikunj <nikunjit645@gmail.com>
2023-03-16 13:04:08 +0530

    login-register module created and some js issue fixed

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -146,400 +146,398 @@ $(function () {
     });
   // DSIGN LOGO
 
-  // $(document).ready(function () {
-  //   $(".design_post_swiper").owlCarousel({
-  //     items: 2.5,
-  //     loop: true,
-  //     autoplay: false,
-  //     autoplayTimeout: 1500,
-  //     margin: 20,
-  //     nav: false,
-  //     dots: true,
-  //     navText: [
-  //       '<span class="fas fa-chevron-left fa-2x"></span>',
-  //       '<span class="fas fa-chevron-right fa-2x"></span>',
-  //     ],
-  //     responsive: {
-  //       320: {
-  //         items: 3.5,
-  //       },
-  //       420: {
-  //         items: 4,
-  //       },
-  //       480: {
-  //         items: 4.5,
-  //       },
-  //       575: {
-  //         items: 5.5,
-  //       },
-  //       768: {
-  //         items: 6.5,
-  //       },
-  //       992: {
-  //         items: 7,
-  //       },
-  //       1024: {
-  //         items: 9,
-  //       },
-  //       1366: {
-  //         items: 11,
-  //       },
-  //       1499: {
-  //         items: 12,
-  //       },
-  //       1700: {
-  //         items: 15,
-  //       },
-  //     },
-  //   });
-  //   // // HIGHLIGHT SWIPER JS
-  //   $(".highlight_swiper").owlCarousel({
-  //     items: 2.5,
-  //     loop: true,
-  //     autoplay: false,
-  //     autoplayTimeout: 1500,
-  //     margin: 20,
-  //     nav: false,
-  //     dots: false,
-  //     navText: [
-  //       '<span class="fas fa-chevron-left fa-2x"></span>',
-  //       '<span class="fas fa-chevron-right fa-2x"></span>',
-  //     ],
-  //     responsive: {
-  //       390: {
-  //         items: 3.5,
-  //       },
-  //       480: {
-  //         items: 4,
-  //       },
-  //       575: {
-  //         items: 5,
-  //       },
-  //       768: {
-  //         items: 6,
-  //       },
-  //       992: {
-  //         items: 7,
-  //       },
-  //       1366: {
-  //         items: 10,
-  //       },
-  //     },
-  //   });
-  //   // LATEST TEMPLATE js
-  //   $(".latest_templates_swiper").owlCarousel({
-  //     items: 2,
-  //     loop: true,
-  //     autoplay: false,
-  //     autoplayTimeout: 1500,
-  //     margin: 20,
-  //     nav: false,
-  //     dots: false,
-  //     navText: [
-  //       '<span class="fas fa-chevron-left fa-2x"></span>',
-  //       '<span class="fas fa-chevron-right fa-2x"></span>',
-  //     ],
-  //     responsive: {
-  //       390: {
-  //         items: 2.5,
-  //       },
-  //       480: {
-  //         items: 3,
-  //       },
-  //       575: {
-  //         items: 3.5,
-  //       },
-  //       768: {
-  //         items: 5,
-  //       },
-  //       992: {
-  //         items: 6,
-  //       },
-  //       1240: {
-  //         items: 6.5,
-  //       },
-  //       1366: {
-  //         items: 8,
-  //       },
-  //     },
-  //   });
+  $(".design_post_swiper").owlCarousel({
+    items: 2.5,
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 1500,
+    margin: 20,
+    nav: false,
+    dots: true,
+    navText: [
+      '<span class="fas fa-chevron-left fa-2x"></span>',
+      '<span class="fas fa-chevron-right fa-2x"></span>',
+    ],
+    responsive: {
+      320: {
+        items: 3.5,
+      },
+      420: {
+        items: 4,
+      },
+      480: {
+        items: 4.5,
+      },
+      575: {
+        items: 5.5,
+      },
+      768: {
+        items: 6.5,
+      },
+      992: {
+        items: 7,
+      },
+      1024: {
+        items: 9,
+      },
+      1366: {
+        items: 11,
+      },
+      1499: {
+        items: 12,
+      },
+      1700: {
+        items: 15,
+      },
+    },
+  });
+  // // HIGHLIGHT SWIPER JS
+  $(".highlight_swiper").owlCarousel({
+    items: 2.5,
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 1500,
+    margin: 20,
+    nav: false,
+    dots: false,
+    navText: [
+      '<span class="fas fa-chevron-left fa-2x"></span>',
+      '<span class="fas fa-chevron-right fa-2x"></span>',
+    ],
+    responsive: {
+      390: {
+        items: 3.5,
+      },
+      480: {
+        items: 4,
+      },
+      575: {
+        items: 5,
+      },
+      768: {
+        items: 6,
+      },
+      992: {
+        items: 7,
+      },
+      1366: {
+        items: 10,
+      },
+    },
+  });
+  // LATEST TEMPLATE js
+  $(".latest_templates_swiper").owlCarousel({
+    items: 2,
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 1500,
+    margin: 20,
+    nav: false,
+    dots: false,
+    navText: [
+      '<span class="fas fa-chevron-left fa-2x"></span>',
+      '<span class="fas fa-chevron-right fa-2x"></span>',
+    ],
+    responsive: {
+      390: {
+        items: 2.5,
+      },
+      480: {
+        items: 3,
+      },
+      575: {
+        items: 3.5,
+      },
+      768: {
+        items: 5,
+      },
+      992: {
+        items: 6,
+      },
+      1240: {
+        items: 6.5,
+      },
+      1366: {
+        items: 8,
+      },
+    },
+  });
 
-  //   // INSTA POST JS
-  //   $(".insta_post_swiper").owlCarousel({
-  //     items: 2,
-  //     loop: true,
-  //     autoplay: false,
-  //     autoplayTimeout: 1500,
-  //     margin: 20,
-  //     nav: false,
-  //     dots: false,
-  //     navText: [
-  //       '<span class="fas fa-chevron-left fa-2x"></span>',
-  //       '<span class="fas fa-chevron-right fa-2x"></span>',
-  //     ],
-  //     responsive: {
-  //       480: {
-  //         items: 2.5,
-  //       },
-  //       575: {
-  //         items: 3.5,
-  //       },
-  //       768: {
-  //         items: 4.5,
-  //       },
-  //       992: {
-  //         items: 5,
-  //       },
-  //       1280: {
-  //         items: 5.5,
-  //       },
-  //       1366: {
-  //         items: 6,
-  //       },
-  //       1499: {
-  //         items: 7.5,
-  //       },
-  //     },
-  //   });
-  //   // INSTA STORY JS
-  //   $(".insta_story_swiper").owlCarousel({
-  //     items: 2,
-  //     loop: true,
-  //     autoplay: false,
-  //     autoplayTimeout: 1500,
-  //     margin: 20,
-  //     nav: false,
-  //     dots: false,
-  //     navText: [
-  //       '<span class="fas fa-chevron-left fa-2x"></span>',
-  //       '<span class="fas fa-chevron-right fa-2x"></span>',
-  //     ],
-  //     responsive: {
-  //       390: {
-  //         items: 2.5,
-  //       },
-  //       480: {
-  //         items: 3,
-  //       },
-  //       575: {
-  //         items: 3.5,
-  //       },
-  //       768: {
-  //         items: 4.5,
-  //       },
-  //       992: {
-  //         items: 5,
-  //       },
-  //       1280: {
-  //         items: 5.5,
-  //       },
-  //       1366: {
-  //         items: 7.5,
-  //       },
-  //     },
-  //   });
-  //   // LOGO JS
-  //   $(".logo_design_swiper").owlCarousel({
-  //     items: 2,
-  //     loop: true,
-  //     autoplay: false,
-  //     autoplayTimeout: 1500,
-  //     margin: 20,
-  //     nav: false,
-  //     dots: false,
-  //     navText: [
-  //       '<span class="fas fa-chevron-left fa-2x"></span>',
-  //       '<span class="fas fa-chevron-right fa-2x"></span>',
-  //     ],
-  //     responsive: {
-  //       480: {
-  //         items: 2.5,
-  //       },
-  //       575: {
-  //         items: 3.5,
-  //       },
-  //       768: {
-  //         items: 4.5,
-  //       },
-  //       992: {
-  //         items: 5,
-  //       },
-  //       1280: {
-  //         items: 5.5,
-  //       },
-  //       1366: {
-  //         items: 6,
-  //       },
-  //       1499: {
-  //         items: 7.5,
-  //       },
-  //     },
-  //   });
-  //   // INVITATION JS
-  //   $(".invitaion_swiper").owlCarousel({
-  //     items: 2.5,
-  //     loop: true,
-  //     autoplay: false,
-  //     autoplayTimeout: 1500,
-  //     margin: 20,
-  //     nav: false,
-  //     dots: false,
-  //     navText: [
-  //       '<span class="fas fa-chevron-left fa-2x"></span>',
-  //       '<span class="fas fa-chevron-right fa-2x"></span>',
-  //     ],
-  //     responsive: {
-  //       480: {
-  //         items: 3,
-  //       },
-  //       575: {
-  //         items: 3.5,
-  //       },
-  //       768: {
-  //         items: 4.5,
-  //       },
-  //       992: {
-  //         items: 5,
-  //       },
-  //       1280: {
-  //         items: 5.5,
-  //       },
-  //       1366: {
-  //         items: 7.5,
-  //       },
-  //     },
-  //   });
-  //   // INSTA LANDSCAPE JS
-  //   $(".insta_landscape_swiper").owlCarousel({
-  //     items: 2,
-  //     loop: true,
-  //     autoplay: false,
-  //     autoplayTimeout: 1500,
-  //     margin: 20,
-  //     nav: false,
-  //     dots: false,
-  //     navText: [
-  //       '<span class="fas fa-chevron-left fa-2x"></span>',
-  //       '<span class="fas fa-chevron-right fa-2x"></span>',
-  //     ],
-  //     responsive: {
-  //       575: {
-  //         items: 2.5,
-  //       },
-  //       768: {
-  //         items: 3.5,
-  //       },
-  //       992: {
-  //         items: 4,
-  //       },
-  //       1366: {
-  //         items: 4.5,
-  //       },
-  //       1441: {
-  //         items: 5.5,
-  //       },
-  //     },
-  //   });
-  //   // INATS PORTRAIT JS
-  //   $(".insta_portrait_swiper").owlCarousel({
-  //     items: 2,
-  //     loop: true,
-  //     autoplay: false,
-  //     autoplayTimeout: 1500,
-  //     margin: 20,
-  //     nav: false,
-  //     dots: false,
-  //     navText: [
-  //       '<span class="fas fa-chevron-left fa-2x"></span>',
-  //       '<span class="fas fa-chevron-right fa-2x"></span>',
-  //     ],
-  //     responsive: {
-  //       390: {
-  //         items: 2.5,
-  //       },
-  //       480: {
-  //         items: 3,
-  //       },
-  //       575: {
-  //         items: 3.5,
-  //       },
-  //       768: {
-  //         items: 4,
-  //       },
-  //       992: {
-  //         items: 5,
-  //       },
-  //       1366: {
-  //         items: 5.5,
-  //       },
-  //       1441: {
-  //         items: 6.5,
-  //       },
-  //     },
-  //   });
-  //   // FB LANDSCAPE JS
-  //   $(".fb_landscape_swiper").owlCarousel({
-  //     items: 2,
-  //     loop: true,
-  //     autoplay: false,
-  //     autoplayTimeout: 1500,
-  //     margin: 20,
-  //     nav: false,
-  //     dots: false,
-  //     navText: [
-  //       '<span class="fas fa-chevron-left fa-2x"></span>',
-  //       '<span class="fas fa-chevron-right fa-2x"></span>',
-  //     ],
-  //     responsive: {
-  //       575: {
-  //         items: 2.5,
-  //       },
-  //       768: {
-  //         items: 3.5,
-  //       },
-  //       992: {
-  //         items: 4,
-  //       },
-  //       1366: {
-  //         items: 4.5,
-  //       },
-  //       1441: {
-  //         items: 5.5,
-  //       },
-  //     },
-  //   });
-  //   // certificate landscape JS
-  //   $(".certificate_landscape_swiper").owlCarousel({
-  //     items: 2,
-  //     loop: true,
-  //     autoplay: false,
-  //     autoplayTimeout: 1500,
-  //     margin: 20,
-  //     nav: false,
-  //     dots: false,
-  //     navText: [
-  //       '<span class="fas fa-chevron-left fa-2x"></span>',
-  //       '<span class="fas fa-chevron-right fa-2x"></span>',
-  //     ],
-  //     responsive: {
-  //       575: {
-  //         items: 2.5,
-  //       },
-  //       768: {
-  //         items: 3.5,
-  //       },
-  //       992: {
-  //         items: 4,
-  //       },
-  //       1366: {
-  //         items: 4.5,
-  //       },
-  //       1441: {
-  //         items: 5.5,
-  //       },
-  //     },
-  //   });
-  // });
+  // INSTA POST JS
+  $(".insta_post_swiper").owlCarousel({
+    items: 2,
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 1500,
+    margin: 20,
+    nav: false,
+    dots: false,
+    navText: [
+      '<span class="fas fa-chevron-left fa-2x"></span>',
+      '<span class="fas fa-chevron-right fa-2x"></span>',
+    ],
+    responsive: {
+      480: {
+        items: 2.5,
+      },
+      575: {
+        items: 3.5,
+      },
+      768: {
+        items: 4.5,
+      },
+      992: {
+        items: 5,
+      },
+      1280: {
+        items: 5.5,
+      },
+      1366: {
+        items: 6,
+      },
+      1499: {
+        items: 7.5,
+      },
+    },
+  });
+  // INSTA STORY JS
+  $(".insta_story_swiper").owlCarousel({
+    items: 2,
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 1500,
+    margin: 20,
+    nav: false,
+    dots: false,
+    navText: [
+      '<span class="fas fa-chevron-left fa-2x"></span>',
+      '<span class="fas fa-chevron-right fa-2x"></span>',
+    ],
+    responsive: {
+      390: {
+        items: 2.5,
+      },
+      480: {
+        items: 3,
+      },
+      575: {
+        items: 3.5,
+      },
+      768: {
+        items: 4.5,
+      },
+      992: {
+        items: 5,
+      },
+      1280: {
+        items: 5.5,
+      },
+      1366: {
+        items: 7.5,
+      },
+    },
+  });
+  // LOGO JS
+  $(".logo_design_swiper").owlCarousel({
+    items: 2,
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 1500,
+    margin: 20,
+    nav: false,
+    dots: false,
+    navText: [
+      '<span class="fas fa-chevron-left fa-2x"></span>',
+      '<span class="fas fa-chevron-right fa-2x"></span>',
+    ],
+    responsive: {
+      480: {
+        items: 2.5,
+      },
+      575: {
+        items: 3.5,
+      },
+      768: {
+        items: 4.5,
+      },
+      992: {
+        items: 5,
+      },
+      1280: {
+        items: 5.5,
+      },
+      1366: {
+        items: 6,
+      },
+      1499: {
+        items: 7.5,
+      },
+    },
+  });
+  // INVITATION JS
+  $(".invitaion_swiper").owlCarousel({
+    items: 2.5,
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 1500,
+    margin: 20,
+    nav: false,
+    dots: false,
+    navText: [
+      '<span class="fas fa-chevron-left fa-2x"></span>',
+      '<span class="fas fa-chevron-right fa-2x"></span>',
+    ],
+    responsive: {
+      480: {
+        items: 3,
+      },
+      575: {
+        items: 3.5,
+      },
+      768: {
+        items: 4.5,
+      },
+      992: {
+        items: 5,
+      },
+      1280: {
+        items: 5.5,
+      },
+      1366: {
+        items: 7.5,
+      },
+    },
+  });
+  // INSTA LANDSCAPE JS
+  $(".insta_landscape_swiper").owlCarousel({
+    items: 2,
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 1500,
+    margin: 20,
+    nav: false,
+    dots: false,
+    navText: [
+      '<span class="fas fa-chevron-left fa-2x"></span>',
+      '<span class="fas fa-chevron-right fa-2x"></span>',
+    ],
+    responsive: {
+      575: {
+        items: 2.5,
+      },
+      768: {
+        items: 3.5,
+      },
+      992: {
+        items: 4,
+      },
+      1366: {
+        items: 4.5,
+      },
+      1441: {
+        items: 5.5,
+      },
+    },
+  });
+  // INATS PORTRAIT JS
+  $(".insta_portrait_swiper").owlCarousel({
+    items: 2,
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 1500,
+    margin: 20,
+    nav: false,
+    dots: false,
+    navText: [
+      '<span class="fas fa-chevron-left fa-2x"></span>',
+      '<span class="fas fa-chevron-right fa-2x"></span>',
+    ],
+    responsive: {
+      390: {
+        items: 2.5,
+      },
+      480: {
+        items: 3,
+      },
+      575: {
+        items: 3.5,
+      },
+      768: {
+        items: 4,
+      },
+      992: {
+        items: 5,
+      },
+      1366: {
+        items: 5.5,
+      },
+      1441: {
+        items: 6.5,
+      },
+    },
+  });
+  // FB LANDSCAPE JS
+  $(".fb_landscape_swiper").owlCarousel({
+    items: 2,
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 1500,
+    margin: 20,
+    nav: false,
+    dots: false,
+    navText: [
+      '<span class="fas fa-chevron-left fa-2x"></span>',
+      '<span class="fas fa-chevron-right fa-2x"></span>',
+    ],
+    responsive: {
+      575: {
+        items: 2.5,
+      },
+      768: {
+        items: 3.5,
+      },
+      992: {
+        items: 4,
+      },
+      1366: {
+        items: 4.5,
+      },
+      1441: {
+        items: 5.5,
+      },
+    },
+  });
+  // certificate landscape JS
+  $(".certificate_landscape_swiper").owlCarousel({
+    items: 2,
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 1500,
+    margin: 20,
+    nav: false,
+    dots: false,
+    navText: [
+      '<span class="fas fa-chevron-left fa-2x"></span>',
+      '<span class="fas fa-chevron-right fa-2x"></span>',
+    ],
+    responsive: {
+      575: {
+        items: 2.5,
+      },
+      768: {
+        items: 3.5,
+      },
+      992: {
+        items: 4,
+      },
+      1366: {
+        items: 4.5,
+      },
+      1441: {
+        items: 5.5,
+      },
+    },
+  });
 
   // IMAGE UPLOAD ANS PREVIEW CSS
   function readURL(input) {
